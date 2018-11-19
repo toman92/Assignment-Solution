5c8d08738a995fb301bd75511139aed51550fa80
Sean Toman <tomansean@gmail.com>
2018-11-19 17:25:48 +0000

    Unfinished

---
+++ src/ie/lyit/flight/Booking.java
@@ -0,0 +1,93 @@
+package ie.lyit.flight;
+
+import java.util.ArrayList;
+
+public class Booking {
+	
+	private Flight outbound;
+	private Flight inbound;
+	private ArrayList<Passenger> passengers;
+	private double totalPrice;
+	
+	private static int count = 1;
+	
+	public Booking() {
+		this.outbound = new Flight();
+		this.inbound = new Flight();
+		this.passengers = new ArrayList<Passenger>();
+		this.totalPrice = 0;
+		count++;
+	}
+	
+	public Booking(Flight outbound, Flight inbound, ArrayList<Passenger> passengers) {
+		this.outbound = outbound;
+		this.inbound = inbound;
+		this.passengers = passengers;
+		calculatePrice();
+		count++;
+	}
+	
+	public void setOutbound(Flight outbound) {
+		this.outbound = outbound;
+	}
+	
+	public void setInbound(Flight inbound) {
+		this.inbound = inbound;
+	}
+	
+	public void setPassengers(ArrayList<Passenger> passengers) {
+		this.passengers = passengers;
+	}
+	
+	public void setTotalPrice() {
+		calculatePrice();
+	}
+	
+	public Flight getOutbound() {
+		return outbound;
+	}
+	
+	public Flight getInbound() {
+		return inbound;
+	}
+	
+	public ArrayList<Passenger> getPassengers() {
+		return passengers;
+	}
+	
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	@Override
+	public String toString() {
+		return "Outbound: " + outbound 
+				+ ", Inbound: " + inbound 
+				+ ", No Passengers: " + passengers.size() 
+				+ ", Total Price: " + totalPrice;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Booking b;
+		if(obj instanceof Booking)
+			b = (Booking) obj;
+		else
+			return false;
+		
+		return this.outbound.equals(b.getOutbound()) && this.inbound.equals(b.getInbound());
+	}
+	
+	public boolean findPassenger(Passenger p) {
+		return passengers.contains(p);
+	}
+	
+	public double calculatePrice() {
+		totalPrice = (outbound.getPrice() + inbound.getPrice()) * passengers.size();
+		return totalPrice;
+	}
+}
---
+++ src/ie/lyit/flight/BookingTest.java
@@ -0,0 +1,75 @@
+package ie.lyit.flight;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookingTest {
+	
+	private Booking b1;
+	private Booking b2;
+
+	@Before
+	public void setUp() throws Exception {
+		// default constructor
+		b1 = new Booking();
+		
+		// Constructor with flights and passengers
+		Flight f1 = new Flight("1", "Donegal", "Dublin", 19, 11, 2018, 12, 40, 15.99);
+		Flight f2 = new Flight("2", "Dublin", "Cork", 18, 11, 2018, 10, 25, 14.99);
+		ArrayList<Passenger> p1 = new ArrayList<>();
+		p1.add(new Passenger("Mr", "Sean", "Toman", 19 ,12 ,1992, 2, true));
+		b2 = new Booking(f1, f2, p1);
+	}
+
+	@Test
+	public void testBookingFlightFlightArrayListOfPassenger() {
+		assertEquals(b2.getOutbound().getFlightNumber(), "1");
+		assertEquals(b2.getInbound().getFlightNumber(), "2");
+		assertEquals(b2.getPassengers().size(), 1);
+	}
+
+	@Test
+	public void testSetOutbound() {
+		b2.setOutbound(new Flight("3", "Belfast", "Dublin", 20, 11, 2018, 11, 30, 16.99));
+		assertEquals(b2.getOutbound().getFlightNumber(), "3");
+	}
+
+	@Test
+	public void testSetInbound() {
+		b2.setInbound(new Flight("4", "Dublin", "London", 21, 11, 2018, 11, 45, 17.99));
+		assertEquals(b2.getInbound().getFlightNumber(), "4");
+	}
+
+	@Test
+	public void testSetPassengers() {
+		ArrayList<Passenger> passTest = new ArrayList<>();
+		passTest.add(new Passenger("Mr", "Ciaran", "Toman", 26, 7, 1989, 1, false));
+		b2.setPassengers(passTest);
+		assertEquals(b2.getPassengers().size(), 1);
+	}
+
+	@Test
+	public void testSetTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		System.out.println(b2);
+	}
+
+	@Test
+	public void testFindPassenger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculatePrice() {
+		fail("Not yet implemented");
+	}
+
+}
